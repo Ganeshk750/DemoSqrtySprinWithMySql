fcc1a60a6e4e83e55e33505819357a18e283eba8
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-22 22:56:35 +0530

    adding jwtUtil and jwtfilter class

--- src/main/java/com/ganesh/util/JwtUtil.java
+++ src/main/java/com/ganesh/util/JwtUtil.java
@@ -47,6 +47,7 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
+    
 
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
